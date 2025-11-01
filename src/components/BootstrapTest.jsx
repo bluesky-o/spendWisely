9433b9636affad13a21ed454c21a3376ee48b613
bluesky <blueskyco005@gmail.com>
2025-11-01 13:25:35 +0530

    setup eslint

--- src/components/BootstrapTest.jsx
+++ src/components/BootstrapTest.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 
 function BootstrapTest() {
-	return (
-		<div className="container" style={{ paddingLeft: "40px" }}>
-			<button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        		Open Modal
-      		</button>
+    return (
+        <div className="container" style={{ paddingLeft: "40px" }}>
+            <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Open Modal
+            </button>
 
-			<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-				<div className="modal-dialog">
-					<div className="modal-content">
-						<div className="modal-header">
-							<h5 className="modal-title" id="exampleModalLabel">Bootstrap Modal</h5>
-						</div>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Bootstrap Modal</h5>
+                        </div>
 
-						<div className="modal-body">
-							Bootstrap test modal
-						</div>
+                        <div className="modal-body">
+                            Bootstrap test modal
+                        </div>
 
-						<div className="modal-footer">
-							<button type="button" data-bs-dismiss="modal">Close</button>
-						</div>
-					</div>
-				</div>
-			</div>
-    	</div>
-	);
+                        <div className="modal-footer">
+                            <button type="button" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default BootstrapTest;
